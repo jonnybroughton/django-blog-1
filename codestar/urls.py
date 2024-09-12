bc9a057e73771f0afeaf256500a58569f9abe376
Jonny Broughton <jonny.broughton08@gmail.com>
2024-09-12 20:13:39 +0000

    fix error with misplaced path

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
